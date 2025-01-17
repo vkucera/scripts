75469b29a3819f74d1c399a7c3444403324bbcfb
Vít Kučera <vit.kucera@cern.ch>
2025-01-17 18:27:00 +0100

    temperature.sh Redirect stderr to null.

--- temperature.sh
+++ temperature.sh
@@ -7,7 +7,7 @@ ncores=$(($(nproc)/2))
 declare -a temp
 
 for i in $(seq 0 $(($ncores-1))); do
-  temp[i]=$(sensors -u | grep "Core $i" -A 1 | grep temp | cut -d " " -f 4)
+  temp[i]=$(sensors -u 2> /dev/null | grep "Core $i" -A 1 | grep temp | cut -d " " -f 4)
 done
 now=$(date +"%Y-%m-%d_%H-%M-%S");
 echo $now ${temp[@]}
