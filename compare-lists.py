8f14658d6c6382a2a6b9fc2907dec945c12195d2
Vít Kučera <vit.kucera@cern.ch>
2025-01-17 19:05:19 +0100

    compare-lists.py Port to Python 3

--- compare-lists.py
+++ compare-lists.py
@@ -11,9 +11,9 @@ fileIn=""
 try:
   nameFileIn = sys.argv[1]
 except IndexError:
-  print "Please specify an input file."
+  print("Please specify an input file.")
   sys.exit()
-print "Input file:", nameFileIn
+print("Input file:", nameFileIn)
 listFiles=[]
 try:
   with open(nameFileIn, "r") as fileIn:
@@ -23,10 +23,10 @@ try:
         listFiles.append(line)
   fileIn.closed
 except IOError:
-  print "Failed to open file: {:s}.".format(nameFileIn)
+  print("Failed to open file: {:s}.".format(nameFileIn))
   sys.exit()
 nFilesTot=len(listFiles)
-print "Files to process:", nFilesTot
+print("Files to process:", nFilesTot)
 
 # Declare the dictionary
 table={}
@@ -35,7 +35,7 @@ listFilesOK=[]
 
 # For each file in the list
 for path in listFiles:
-  print "Processing file: {:s} ({:d})".format(path, indexFile)
+  print("Processing file: {:s} ({:d})".format(path, indexFile))
   try:
     with open(path, "r") as fileListOfLines:
       # For each line in the file
@@ -50,23 +50,22 @@ for path in listFiles:
     indexFile+=1
     listFilesOK.append(path)
   except IOError:
-    print "Failed"
+    print("Failed")
     continue
 
-print "Processed files:", indexFile
+print("Processed files:", indexFile)
 
 # Print out a table specifying occurence of each line (table row) in each file (table column).
 header=""
 for i in listFilesOK:
   header+="\t{}".format(i)
-print header
+print(header)
 header=""
 for i in range(0, indexFile):
   header+="\t{}".format(i)
-print header
+print(header)
 for key in sorted(table.keys()):
   line=key
   for i in range(0, indexFile):
     line+="\t{}".format( 1 if i in table[key] else 0 )
-  print line
-
+  print(line)
