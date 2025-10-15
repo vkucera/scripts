0968132f4a9963625caaddb1a94edca270893aba
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2025-10-15 14:16:02 +0200

    shred scripts: Add quotes

--- shreddir.sh
+++ shreddir.sh
@@ -5,6 +5,6 @@
 [ -d "$1" ] || { echo "Directory $1 does not exist"; exit 1; }
 
 echo "Shredding directory: $1"
-find "$1" -depth -type f -exec "$(dirname $(realpath $0))"/shred.sh {} \; && rm -rf "$1"
+find "$1" -depth -type f -exec "$(dirname "$(realpath "$0")")"/shred.sh {} \; && rm -rf "$1"
 
 exit 0
