1e8fd107b3c9dcc0760e2395e69b50d5362ade6b
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-03-15 19:17:39 +0100

    Add function for scheduled restart

--- monitor_system.sh
+++ monitor_system.sh
@@ -14,6 +14,9 @@ FILE_BASE="$HOME/monitor-logs"
 
 TITLE_WARNING="**:warning: $HOSTNAME warning**\n"
 
+TIME_RESTART="2026-03-15 08:00:00"
+TIME_REMINDER_RESTART="10" # minutes before restart to send a reminder
+
 # Maximum disk usage (%)
 DISK_LIMIT_SOFT=95
 DISK_LIMIT_HARD=98
@@ -22,7 +25,7 @@ DISK_LIMIT_HARD=98
 MEMORY_LIMIT_SOFT="10"
 MEMORY_LIMIT_HARD="5"
 
-notify_mattermost() {
+notify() {
   local text="$1"
   # echo -e "$text"
   # return 0
@@ -40,7 +43,7 @@ check_memory() {
   memory_available="$(free | grep Mem | awk '{print $7}')"
   local notify=0
   if [[ "$HARD" -eq 1  ]]; then
-    local logfile="${FILE_BASE}/${path}/memory_full"
+    local logfile="${FILE_BASE}/memory_full"
     memory_min=$((MEMORY_LIMIT_HARD * memory_total / 100))
     if ((memory_available < memory_min)); then
       [[ -f "${logfile}" ]] && return 0
@@ -61,7 +64,7 @@ check_memory() {
     local text="${message}\n"
     text+="\n| user | usage [GiB] |\n|---|---|\n"
     text+="$(ps S -e --no-headers -o user:20,pss | awk '{m[$1] += $2; m["total"] += $2} END{for(u in m) {print u, m[u]}}' | sort -rnk 2 | head -5 | awk '{printf "| %s | %.0f |\\n", $1, $2/1048576}')"
-    notify_mattermost "$text"
+    notify "$text"
   fi
 }
 
@@ -75,14 +78,14 @@ check_disk() {
     mkdir -p "${FILE_BASE}/${path}"
     if ((disk_use > DISK_LIMIT_HARD)); then
       [[ -f "${logfile}" ]] && return 0
-      notify_mattermost "${TITLE_WARNING}@all Disk in ${path} is **FULL**: ${disk_use} % (threshold ${DISK_LIMIT_SOFT} %)"
+      notify "${TITLE_WARNING}@all Disk in ${path} is **FULL**: ${disk_use} % (threshold ${DISK_LIMIT_SOFT} %)"
       date +"%F_%H-%M-%S" > "${logfile}"
     else
       rm -f "${logfile}"
     fi
   else
     if ((disk_use > DISK_LIMIT_SOFT)); then
-      notify_mattermost "${TITLE_WARNING}@all Disk usage in ${path} is high: ${disk_use} % (threshold ${DISK_LIMIT_SOFT} %)"
+      notify "${TITLE_WARNING}@all Disk usage in ${path} is high: ${disk_use} % (threshold ${DISK_LIMIT_SOFT} %)"
     fi
   fi
 }
@@ -95,7 +98,35 @@ check_processes() {
   users="$(echo -e "$table" | awk '(NR > 1 && $0 != "") {users[$1] = 1} END{for (u in users) {printf "@%s ", u}}')"
   local header="${TITLE_WARNING}Suspicious old processes were found. Please check. ${users}\n\n\`\`\`\n"
   local footer="\`\`\`"
-  notify_mattermost "${header}${table}${footer}"
+  notify "${header}${table}${footer}"
+}
+
+restart() {
+  local logfile="${FILE_BASE}/restart"
+  local time_restart
+  local now
+  local before_restart
+  time_restart="$(date +"%s" -d "${TIME_RESTART}")"
+  now="$(date +"%s")"
+  before_restart=$((now < time_restart))
+  if [[ ! -f "${logfile}" ]]; then
+    [[ $before_restart -eq 0 ]] && return 0
+    notify "${TITLE_WARNING}@all Restart of the system has been scheduled for ${TIME_RESTART}."
+    echo "${TIME_RESTART}" > "${logfile}"
+    return 0
+  fi
+  if [[ $before_restart -eq 1 ]]; then
+    if ((time_restart - now <= 60 * TIME_REMINDER_RESTART)) && (($(wc -l < "${logfile}") == 1)); then
+      notify "${TITLE_WARNING}@all Restart of the system imminent at ${TIME_RESTART}."
+      echo "${TIME_RESTART}" >> "${logfile}"
+    fi
+  elif ((time_restart <= $(date +"%s" -d "$(uptime -s)"))); then
+    notify "${TITLE_WARNING}@all Restart of the system at ${TIME_RESTART} was successful."
+    rm -f "${logfile}"
+  else
+    notify "${TITLE_WARNING}@all Restarting the system now."
+    systemctl reboot
+  fi
 }
 
 case "${CHECK}" in
@@ -109,5 +140,8 @@ case "${CHECK}" in
   ;;
   "processes")
     check_processes
+  ;;
+  "restart")
+    restart
   ;;
 esac
